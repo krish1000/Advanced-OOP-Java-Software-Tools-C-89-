c8d93e76ef032556c07ae6c160aff1a61062677b
Krishaanth <krish100@my.yorku.ca>
2019-09-26 11:21:38 -0400

    lab3B finihed
    ree

--- 2031Lab3/lab3B.c
+++ 2031Lab3/lab3B.c
@@ -6,31 +6,39 @@
 * York Student #: 216463150
 ****************************************/
 
+/* IMPORTANT*** ASSUMING ALL INPUTS ARE VALID
+ * a.k.a all char arrays are INTEGERS!
+ */
 #include <stdio.h>
 
 #define SIZE 10
 
-
+/**declaring methods */
 int my_atoi (char c[]); /*added dis */
+int length (char arr[]);
+int isQuit(char arr[]);
+int power(int base, int n); /*helper function*/
 
 int main(){
   int a,b;
   char arr [SIZE];
 
-  printf("Enter a postive number or 'quit': " );
-  scanf("%s", arr);
-  while(   )
-  {
-    printf("%s", arr);
+  while(isQuit(arr) != 1)  //you could try strcmp() command
+    {
+	  	  printf("Enter a postive number or 'quit': " );
+	  	  scanf("%s", arr);
+	  	/* While loop used to be here*/
+	  	  if(isQuit(arr) == 1) {
+	  		  return 0;
+	  		  /* if return 0  doesn't work try break;*/
+	  	  }
+	  	  printf("%s", arr);
 
-    a = atoi(arr);
-    printf("atoi:    %d (%#o, %#X)\t\t%d\t\t%d\n", a,a,a, a*2, a*a);
+	  	  a = atoi(arr);
+	  	  printf("atoi:    %d (%#o, %#X)\t\t%d\t\t%d\n", a,a,a, a*2, a*a);
 
-    b = my_atoi(arr);
-    printf("my_atoi: %d (%#o, %#X)\t\t%d\t\t%d\n", b,b,b, b*2, b*b);
-
-
-
+	  	  b = my_atoi(arr);
+	  	  printf("my_atoi: %d (%#o, %#X)\t\t%d\t\t%d\n", b,b,b, b*2, b*b);
   }
 
   return 0;
@@ -44,6 +52,64 @@ int main(){
 
 int my_atoi (char c[])
 {
-
-
+	/*Takes he length of the char array, subtracts by 1, cuz it makes it a higher index */
+	int length1 = length(c) -1 ;
+	int i = 0; /*declaring for loop*/
+	int x = 0; /*declaring for final num*/
+	while (c[i] != '\0'){
+		if(c[i] == '\0'){
+			return x;
+		}
+		/*Re-adds original num then takes power of each increasing base 10
+		  then multiplies to the char value subtracted by 0, assuming
+		  everything is an integer value, (minus 48) to get integer value */
+		x = x +  power(10, i)*(c[length1] - '0');
+		length1--;
+		i++;
+	}
+	/*
+	for (i = length1; i > 0; i--){
+		x
+	}
+	*/
+	return x;
 }
+
+/*Made powerfunction cuz math.pow isn't allowed*/
+int power(int base, int n){
+	int i = 1;
+	int x = base;
+	if (n == 0){
+		return 1;
+	} else if (n == 1) {
+		return base;
+	}
+	for(i = 1; i<n; i++){
+		x *= base;
+	}
+	return x;
+}
+
+/* took from lab2E.c */
+int length(char arr[])
+{
+	int count;
+	count = 0;
+
+	while (arr[count] != '\0'){
+		count++;
+	}
+	return count;
+}
+
+/* try using strcmp() after; quit function taken from lab2E.c */
+int isQuit (char arr[])
+{
+ if (arr[0]=='q' && arr[1]=='u' && arr[2]=='i' && arr[3]=='t') {
+    return 1;
+ }
+ else{
+    return 0;
+ }
+}
+
---
+++ 2031Lab3/lab3conv.c
@@ -0,0 +1,46 @@
+/***************************************
+* EECS2031B – Lab2 *
+* Author: Manoharan, Krishaanth *
+* Email: krish100@my.yorku.ca *
+* eecs_username: krish100 *
+* York Student #: 216463150
+****************************************/
+#include <stdio.h>
+
+
+
+main(){
+
+ int op1; float op2;  char operator;
+ float resu, resu2, resu3;
+
+ // conversion in arithemetic
+ printf("3.0*9/2/4=%f  9/2*3.0/4=%f  9*3/2*3.0/4=%f\n", 3.0*9/2/4, 9/2*3.0/4, 9/2*3.0/4);
+
+ // conversion in assignment
+ float f  = 3.96;
+ float f2 = 3.03;
+ int i = f; int j = f2;
+ printf("i: %d  j: %d\n", i, j);
+
+ .....
+
+
+
+}
+
+float calIntFloat(int op1, char operator, float op2)
+{
+
+}
+
+float calIntInt(int op1, char operator, int op2)
+{
+
+}
+
+float calFloatFloat(float op1, char operator, float op2)
+{
+
+}
+
