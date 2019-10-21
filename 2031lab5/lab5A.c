4cd4673df7a9735f2eaa11e970b16fe2c6189751
Krishaanth <krish100@my.yorku.ca>
2019-10-21 14:39:58 -0400

    2031lab5 stuff

---
+++ 2031lab5/lab5A.c
@@ -0,0 +1,47 @@
+/***************************************
+* EECS2031B – Lab 5 *
+* Author: Krishaanth, Manoharan*
+* Email: krish100@my.yorku.ca *
+* eecs_num: krish100 *
+* Yorku #: 216463150
+****************************************/
+#include <stdio.h>
+
+void swapIncre(int*, int*, int*);
+
+int main( ) {
+  int a, b,c;
+
+  /* Input three integers*/
+  scanf("%d %d %d", &a, &b,&c);
+  while(a != -1) {
+     printf("Original inputs:   a:%-4d  b:%-4d  c:%-4d\n", a, b,c);
+//     swapIncre(a,b,c); //given
+     swapIncre(&a,&b,&c);
+     printf("Rearranged inputs: a:%-4d  b:%-4d  c:%-4d\n\n", a, b,c);
+
+     /* read again */
+     scanf("%d %d %d", &a, &b,&c);
+   }
+
+}
+
+//void swapIncre(int x, int y, int z){ //given
+//int temp = x;
+//  x = z;
+//  z = temp;
+//
+//  x += 100;
+//  y *= 2;
+//}
+
+void swapIncre(int *x, int *y, int *z){
+   int temp = *x;
+   *x = *z;
+   *z = temp;
+
+   *x += 100;
+   *y *= 2;
+}
+
+
---
+++ 2031lab5/lab5pow.c
@@ -0,0 +1,64 @@
+/***************************************
+* EECS2031B – Lab 5 *
+* Author: Krishaanth, Manoharan*
+* Email: krish100@my.yorku.ca *
+* eecs_num: krish100 *
+* Yorku #: 216463150
+****************************************/
+#include <stdio.h>
+
+
+#include <math.h>
+
+//....
+int total = 0;
+
+double my_pow(double, double);
+
+int main(int argc, char *argv[])
+{
+   int base, power;
+   printf("enter base and power: ");
+   scanf("%d %d", &base, &power);
+   //total = base;
+   while (base != -100){
+        double res = pow(base, power);
+        double res2 = my_pow(base, power);
+        printf("pow:    %.4f\n", res);
+        printf("my_pow: %.4f\n", res2);
+
+
+         //....
+        printf("enter base and power: ");
+        scanf("%d %d", &base, &power);
+       // total = base;
+        total = 0;
+    }
+    return 0;
+}
+
+// this function should be RECURSIVE
+// should not use any loop here
+double my_pow(double base, double p)
+{
+	//int total;
+//	if (p == 0){
+//		return total;
+//
+//	}
+	if(p>0) {
+		total += base*base;
+		//printf("\npoo: %f", base);
+		if ((p-1) == 0){
+			return total;
+		}
+		my_pow(base, p-1);
+	} else if (p < 0) {
+		total += base/base;
+		if (p+1 == 0){
+			return total;
+		}
+		my_pow(base, p+1);
+	}
+	return 1;
+}
