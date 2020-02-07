82d9d41b36a75b53ae1ee9e4013aca409f47057d
Krish <54220286+krish1000@users.noreply.github.com>
2020-02-06 19:47:29 -0500

    Update function.c

--- 2031Lab4/function.c
+++ 2031Lab4/function.c
@@ -1,9 +1,6 @@
 /***************************************
 * EECS2031B – Lab4 *
 * Author: Manoharan, Krishaanth *
-* Email: krish100@my.yorku.ca *
-* eecs_username: krish100 *
-* York Student #: 216463150
 ****************************************/
 #include <stdio.h>
 
